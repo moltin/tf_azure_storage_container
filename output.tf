3a064cef1586021a0b3cafe14f4cb1c7e9139415
Israel Sotomayor <sotoisra24@gmail.com>
2016-07-31 22:05:42 +0200

    fix: missing output value

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "id" {
     value = "${azurerm_storage_container.mod.properties}"
 }
 
+output "name" {
+    value = "${azurerm_storage_container.mod.name}"
+}
+
 output "properties" {
     value = "${azurerm_storage_container.mod.properties}"
 }
