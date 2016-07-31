925f929ff66c0e975437a77f1884581aa1a5e31b
Israel Sotomayor <sotoisra24@gmail.com>
2016-07-31 12:23:24 +0200

    fix: variable name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_container" "mod" {
     name = "${var.name}"
-    resource_group_name = "${var.resource_group.name}"
+    resource_group_name = "${var.resource_group_name}"
     storage_account_name = "${var.storage_account_name}"
     container_access_type = "${var.container_access_type}"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "name" {}
-variable "resource_group" {}
+variable "resource_group_name" {}
 variable "storage_account_name" {}
 variable "container_access_type" {}
